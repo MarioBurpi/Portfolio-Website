b154487ab41a47f60e0e2d274be7cf1a8129dd53
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-08-08 22:59:00 +0200

    About-page: incomplete.
    Styling - bootstrap cards
    
    add some personal information.
    
    Need to be structured and add graphics/stars rating skills

--- src/app/screens/about-scr/about-scr.component.ts
+++ src/app/screens/about-scr/about-scr.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-scr',
   templateUrl: './about-scr.component.html',
-  styleUrls: ['./about-scr.component.css']
+  styleUrls: ['./about-scr.component.css'], 
 })
+
 export class AboutScrComponent implements OnInit {
+
+  
 
   constructor() { }
 
