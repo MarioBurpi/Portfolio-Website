c24dd781992ee100dcdba1a2c442e2c2dc8a5e02
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-08-08 09:02:32 +0200

    did app-routing module, set the routerLinks at each navbar link and tagged the <router-outlet> at the main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScrComponent } from './screens/home-scr/home-scr.component';
+import { WorkScrComponent } from './screens/work-scr/work-scr.component';
+import { AboutScrComponent } from './screens/about-scr/about-scr.component';
+import { ContactScrComponent } from './screens/contact-scr/contact-scr.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeScrComponent },
+  { path: 'work', component: WorkScrComponent },
+  { path: 'about', component: AboutScrComponent },
+  { path: 'contact', component: ContactScrComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+
+ }
